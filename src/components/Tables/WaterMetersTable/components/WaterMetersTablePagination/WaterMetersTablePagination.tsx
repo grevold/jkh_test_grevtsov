913ef03927c6f39ddf37b1fff39bdc2a267a3c26
Алексей <87669945+grevold@users.noreply.github.com>
2026-04-12 18:03:20 +0300

    add parsing query params (#3)

--- src/components/Tables/WaterMetersTable/components/WaterMetersTablePagination/WaterMetersTablePagination.tsx
+++ src/components/Tables/WaterMetersTable/components/WaterMetersTablePagination/WaterMetersTablePagination.tsx
@@ -1,20 +1,60 @@
+import { useMemo } from "react";
 import s from "./WaterMetersTablePagination.module.css";
 
 interface Props {
+  count: number;
   limit: number;
   offset: number;
+  onChangeOffset: (newOffset: number) => void;
 }
 
-export function WaterMetersTablePagination({ limit, offset }: Props) {
+export function WaterMetersTablePagination({
+  count,
+  limit,
+  offset,
+  onChangeOffset,
+}: Props) {
+  const paginationButtons = useMemo(() => {
+    const result = [];
+    for (let i = 1; i <= Math.floor(count / limit); i++) {
+      result.push(i);
+    }
+    return result;
+  }, [count]);
   return (
     <div className={s.root}>
       <ul className={s.container_navigation}>
-        <li className={s.pagination_button}>1</li>
-        <li className={s.pagination_button}>1</li>
-        <li className={s.pagination_button}>1</li>
-        <li className={s.pagination_button}>1</li>
-        <li className={s.pagination_button}>1</li>
+        <li
+          key={`button_${1}`}
+          className={s.pagination_button}
+          onClick={() => onChangeOffset(0)}
+        >
+          1
+        </li>
+        <li
+          key={`button_${2}`}
+          className={s.pagination_button}
+          onClick={() => onChangeOffset(1)}
+        >
+          2
+        </li>
       </ul>
     </div>
   );
+}
+
+{
+  /* <div className={s.root}>
+      <ul className={s.container_navigation}>
+        {paginationButtons.map((button, index) => (
+          <li
+            key={`button_${button}`}
+            className={s.pagination_button}
+            onClick={() => onChangeOffset(button)}
+          >
+            {button}
+          </li>
+        ))}
+      </ul>
+    </div> */
 }
