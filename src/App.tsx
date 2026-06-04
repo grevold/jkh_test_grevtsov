93ae2fae605901acc6d347bee96667d7f5e0df26
Гревцов Алексей Александрович <GrevcovAA@gov74.ru>
2026-06-04 16:49:46 +0500

    create new project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { LayoutMain } from './components/Layouts/LayoutMain/LayoutMain';
-import { MainPage } from './pages/MainPage/MainPage';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Player } from "./components/Player/Player";
 
-const queryClient = new QueryClient();
 
 function App() {
+  const styles = {
+    color: 'green',
+    display: 'flex',
+    "flex-direction": 'raw',
+    "flex-wrap":"wrap",
+    "gap":"50px"
+  };
+  const VIDEOS = ["http://minstroy:A21GbztRX@81.90.211.34:33661/cameras/58/streaming/main.mp4", "http://minstroy:A21GbztRX@81.90.211.34:33661/cameras/24/streaming/main.mp4", "http://minstroy:A21GbztRX@81.90.211.34:33661/cameras/35/streaming/main.mp4", "http://minstroy:A21GbztRX@81.90.211.34:33661/cameras/20/streaming/main.mp4", "http://minstroy:A21GbztRX@81.90.211.34:33661/cameras/43/streaming/main.mp4"]
   return (
-    <QueryClientProvider client={queryClient}>
-      <Router>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <LayoutMain>
-                <MainPage />
-              </LayoutMain>
-            }
-          />
-        </Routes>
-      </Router>
-    </QueryClientProvider>
+    <><h1>hello</h1>
+      <div style={styles} >{VIDEOS.map((src) => (<Player src={src}></Player>))}</div>
+
+      {/* <div>{VIDEOS.map((src) => (<Player src={src}></Player>))}</div> */}
+    </>
+
   );
 }
 
---
+++ src/components/Grid/Grid.tsx
@@ -0,0 +1,5 @@
+import s from "./Grid.module.css"
+
+export function Grid() {
+    return (<div className={s.root}></div>)
+}
